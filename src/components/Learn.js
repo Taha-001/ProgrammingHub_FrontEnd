452005ec4773257a0e0b9b88736d0566a93e5326
Aryan <aryan04@somaiya.edu>
2021-09-28 12:43:24 +0530

    Basic Calendar,Changed routing,redux libraries

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -1,14 +1,17 @@
 import React from "react";
 import SideDrawer from "./SideDrawer";
+import { Route } from "react-router-dom";
 import TopNavigationBar from "./TopNavigationBar";
 import Courses from "./Courses";
+import Calendar from "./calendar/Calender";
 
 const Learn = () => {
   return (
     <div>
       <TopNavigationBar />
-      <Courses />
       <SideDrawer />
+      <Route path="/learn" component={Courses} />
+      <Route path="/calendar" component={Calendar} />
     </div>
   );
 };
