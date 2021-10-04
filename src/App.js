bde9cdb118c649def39c72c72fc0d9bd625ce56b
Shruti <sss22@somaiya.edu>
2021-10-04 14:34:14 +0530

    added progress page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import LoginPage from "./components/LoginPage";
 import SignUpPage from "./components/SignUpPage";
 import Learn from "./components/Learn";
 import Groups from "./components/Groups";
+import ProgressPage from "./components/ProgressPage";
 
 function App() {
   return (
     <Fragment>
       <BrowserRouter>
-      <Switch>
-      <Route exact path="/" component={LoginPage} />
-      {/* <LoginPage /> */}
-      <Route path="/signup" component={SignUpPage} />
-      <Route path="/learn" component={Learn} />
-      <Route path="/groups" component={Groups}/>
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/learn" component={Learn} />
+          <Route path="/groups" component={Groups} />
+          <Route path="/progress" component={ProgressPage} />
+        </Switch>
       </BrowserRouter>
     </Fragment>
   );
