184eca4237105a611e68ead813dcd042e5cbba4e
Aryan <aryan04@somaiya.edu>
2021-10-11 16:14:29 +0530

    logout

--- src/Redux/Action/actions.js
+++ src/Redux/Action/actions.js
@@ -18,11 +18,64 @@ const signupFail = (error) => ({
 
 export const signupInitiate = (email, password, displayName) => {
   return function (dispatch) {
-    dispatch(signupStart);
-    createUserWithEmailAndPassword(auth, email, password)
+    dispatch(signupStart());
+    auth
+      .createUserWithEmailAndPassword(email, password)
       .then(({ user }) => {
         dispatch(signupSuccess(user));
       })
       .catch((error) => dispatch(signupFail(error.message)));
+  };
+};
+
+//login
+const loginStart = () => ({
+  type: types.LOGIN_START,
+});
+
+const loginSuccess = (user) => ({
+  type: types.LOGIN_SUCCESS,
+  payload: user,
+});
+
+const loginFail = (error) => ({
+  type: types.LOGIN_FAIL,
+  payload: error,
+});
+
+export const loginInitiate = (email, password) => {
+  return function (dispatch) {
+    dispatch(loginStart());
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        dispatch(loginSuccess(user));
+      })
+      .catch((error) => dispatch(loginFail(error.message)));
+  };
+};
+
+//LOGOUT
+
+const loginStart = () => ({
+  type: types.LOGOUT_START,
+});
+
+const loginSuccess = (user) => ({
+  type: types.LOGOUT_SUCCESS,
+});
+
+const loginFail = (error) => ({
+  type: types.LOGOUT_FAIL,
+  payload: error,
+});
+
+export const logoutInitiate = () => {
+  return function (dispatch) {
+    dispatch(logoutStart());
+    auth
+      .signOut()
+      .then((resp) => dispatch(logoutSuccess()))
+      .catch((error) => dispatch(logoutFail(error.message)));
   };
 };
